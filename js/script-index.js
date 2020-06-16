adcb7a64391d9d0bd92cfdff77d33eba17d7c876
pabloyanezb <pabloyanezbarriga@gmail.com>
2020-06-16 16:43:40 -0400

    Etapa 5: Completada función renderActivities

--- js/script-index.js
+++ js/script-index.js
@@ -2,28 +2,13 @@ $(document).ready( function(){
 
 	//La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
 	renderHighlightedRecipes(recipesArray);
-
+	renderActivities(activities);
 });
 
 
 
 
 
-/*
-* Función que se encarga de pintar todas las actividades
-*/
-function renderActivities(activitiesArray) {
-	console.log('Activities: ', activitiesArray);
-}
-
-/*
-* Función que se encarga de pintar una actividad
-* Aqui se tiene que crear el HTML que esta en el 
-* archivo "templates/templates-activity.html"
-*/
-function renderActivity(recipe) {
-	
-}
 // Etapa 1
 $(function(){
 	if (window.location.pathname == "/nivelacion-td-covid/index.html") {
@@ -42,7 +27,7 @@ $(function(){
 });
 // Etapa 2
 $(function printNews () {
-	$('.callout-news>p').html('NUEVAS NOTICIAS');
+	$('.callout-news>p').html('NUEVAS RECETAS');
 });
 // Etapa 3
 /*
@@ -52,9 +37,9 @@ $(function printNews () {
 function renderHighlightedRecipes(recipesArray) {
 	console.log('Recipes: ', recipesArray);
 	for(var i=0; i<recipesArray.length-1; i++) {
-		  if(recipesArray[i].highlighted === true){
+		if(recipesArray[i].highlighted === true){
 			renderRecipe(recipesArray[i]);
-		  }
+		}
 	}
 }
 // Etapa 4
@@ -66,18 +51,43 @@ function renderHighlightedRecipes(recipesArray) {
 */
 function renderRecipe(recipe) {
 	console.log('Voy a pintar la receta: ', recipe);
+	
 	$('.list-recipes').append(`
 	<a class="item-recipe" href="#">
-		<span class="attribution">
-		<span class="title-recipe">${recipe.title} </span>
-		<span class="metadata-recipe">
-			<span class="author-recipe">${recipe.source.name}</span>
-			<span class="bookmarks-recipe">
-			<span class="icon-bookmark"></span> 
-			</span>
-		</span>
-		</span>
+	<span class="attribution">
+	<span class="title-recipe">${recipe.title} </span>
+	<span class="metadata-recipe">
+	<span class="author-recipe">${recipe.source.name}</span>
+	<span class="bookmarks-recipe">
+	<span class="icon-bookmark"></span> 
+	</span>
+	</span>
+	</span>
 	
-		<img src= "./img/recipes/320x350/${recipe.name}.jpg" />
+	<img src= "./img/recipes/320x350/${recipe.name}.jpg" />
 	</a>`);
 }
+// Etapa 5
+/*
+* Función que se encarga de pintar todas las actividades
+*/
+function renderActivities(activitiesArray) {
+	console.log('Activities: ', activitiesArray);
+
+	if (activitiesArray.length > 0) {
+		$('.wrapper-message').hide();
+	}
+	for(var i=0; i<activitiesArray.length-1; i++) {
+		renderActivity(activitiesArray[i]);		
+  }
+}
+
+/*
+* Función que se encarga de pintar una actividad
+* Aqui se tiene que crear el HTML que esta en el 
+* archivo "templates/templates-activity.html"
+*/
+function renderActivity(activities) {
+	
+}
+
