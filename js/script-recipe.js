1bab236df411fb978ab98ca1ad1f262fe68a9b7b
pabloyanezb <pabloyanezbarriga@gmail.com>
2020-06-18 02:21:19 -0400

    Etapa 8: Completada navegación de la página con pestañas .js-show-recipe y .js-show-make

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -5,3 +5,11 @@ $(document).ready( function(){
 	// Etapa 7
 	console.log('El documento HTML está listo');
 });
+
+// Etapa 8
+$('.js-show-recipe').on('click', function() {
+	$('.page.recipe').removeClass('make');
+});
+$('.js-show-make').on('click', function() {
+	$('.page.recipe').addClass('make');
+});
