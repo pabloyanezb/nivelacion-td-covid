b529f00843e49c60b50bc09f8ecd2166328cdb50
pabloyanezb <pabloyanezbarriga@gmail.com>
2020-06-18 01:56:57 -0400

    Etapa 6: Función renderActivity ahora pinta las actividades

--- js/script-index.js
+++ js/script-index.js
@@ -54,17 +54,17 @@ function renderRecipe(recipe) {
 	
 	$('.list-recipes').append(`
 	<a class="item-recipe" href="#">
-	<span class="attribution">
-	<span class="title-recipe">${recipe.title} </span>
-	<span class="metadata-recipe">
-	<span class="author-recipe">${recipe.source.name}</span>
-	<span class="bookmarks-recipe">
-	<span class="icon-bookmark"></span> 
-	</span>
-	</span>
-	</span>
+		<span class="attribution">
+		<span class="title-recipe">${recipe.title} </span>
+			<span class="metadata-recipe">
+				<span class="author-recipe">${recipe.source.name}</span>
+				<span class="bookmarks-recipe">
+					<span class="icon-bookmark"></span> 
+				</span>
+			</span>
+		</span>
 	
-	<img src= "./img/recipes/320x350/${recipe.name}.jpg" />
+		<img src= "./img/recipes/320x350/${recipe.name}.jpg" />
 	</a>`);
 }
 // Etapa 5
@@ -73,21 +73,40 @@ function renderRecipe(recipe) {
 */
 function renderActivities(activitiesArray) {
 	console.log('Activities: ', activitiesArray);
-
+	
 	if (activitiesArray.length > 0) {
 		$('.wrapper-message').hide();
 	}
-	for(var i=0; i<activitiesArray.length-1; i++) {
-		renderActivity(activitiesArray[i]);		
-  }
+	for (var i=0; i<activitiesArray.length; i++) {
+		renderActivity(activitiesArray[i]);
+	}
 }
-
+// Etapa 6
 /*
 * Función que se encarga de pintar una actividad
 * Aqui se tiene que crear el HTML que esta en el 
 * archivo "templates/templates-activity.html"
 */
 function renderActivity(activities) {
-	
+	$('.list-activities').append(`
+		<a href="#" class="item-activity">
+
+			<span class="attribution">
+			
+				<span class="avatar">
+					<img src="${activities.userAvatar}" class="image-avatar">
+				</span>
+				
+				<span class="meta">
+					<span class="author">${activities.userName}</span> made 
+					<span class="recipe">${activities.recipeName}</span>: ${activities.text}
+					<span class="location">&mdash;${activities.place}</span>
+				</span>
+			
+			</span>
+		
+			<div class="bg-image" style="background-image: url('${activities.image}');"></div>
+		
+		</a>`);
 }
 
